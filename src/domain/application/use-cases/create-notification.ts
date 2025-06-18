90354fb7ab10a0ad469aee49e4248eef8f025573
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-18 17:55:32 -0300

    feat: first iteration of create notification use case

--- src/domain/application/use-cases/create-notification.ts
+++ src/domain/application/use-cases/create-notification.ts
@@ -1,1 +1,29 @@
-// TODO: Create notification USE CASE
+import { Notification } from '@/domain/entities/notification'
+import { NotificationRepository } from '../repositories/notification-repository'
+import { Context } from '@/domain/value-objects/context'
+import { UseCaseResponse } from '@/types/user-case-response'
+
+interface CreateNotificationUseCaseRequest {
+  recipientId: string
+  context: Context
+}
+
+type CreateNotificationUseCaseResponse = UseCaseResponse<Notification>
+
+export class CreateNotificationUseCase {
+  constructor(private repository: NotificationRepository) {}
+
+  async execute({
+    context,
+    recipientId,
+  }: CreateNotificationUseCaseRequest): Promise<CreateNotificationUseCaseResponse> {
+    const notification = new Notification({
+      context,
+      recipientId,
+    })
+
+    await this.repository.create(notification)
+
+    return [notification, null]
+  }
+}
