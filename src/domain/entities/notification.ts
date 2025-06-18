b1a67a0f3c147fc927d31225e032b84ee06b1705
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-18 15:51:32 -0300

    feat: create main entity

---
+++ src/domain/entities/notification.ts
@@ -0,0 +1,34 @@
+import { Optional } from '@/types/optional'
+import { EntityID } from '@/value-objects/entity-id'
+import { Context } from '../../value-objects/context'
+
+interface NotificationProps {
+  recipientId: string
+  createdAt: Date
+  readAt?: Date | null
+  context: Context
+}
+
+export class Notification {
+  private id: EntityID
+  readonly recipientId: string
+  private createdAt: Date
+  private readAt?: Date | null
+  private context: Context
+
+  constructor(props: Optional<NotificationProps, 'createdAt'>, id?: EntityID) {
+    this.id = id ?? new EntityID()
+    this.recipientId = props.recipientId
+    this.createdAt = props.createdAt || new Date()
+    this.readAt = props.readAt
+    this.context = props.context
+  }
+
+  public read() {
+    this.readAt = new Date()
+  }
+
+  private test() {
+    console.log(this.context.getResourceName())
+  }
+}
