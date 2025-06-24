3bba6b72e4d77790978eaa59457a632fdbbc9271
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-24 12:08:01 -0300

    feat: fetch notification usecase

--- src/domain/application/repositories/notification-repository.ts
+++ src/domain/application/repositories/notification-repository.ts
@@ -1,12 +1,12 @@
 import { Notification } from '@/domain/entities/notification'
-import { Resource } from '@/domain/value-objects/resource'
+import { MetadataType } from '@/domain/value-objects/metadata/metadata-types'
 
 export abstract class NotificationRepository {
   abstract findManyByUserId(idUser: string): Promise<Notification[]>
   abstract getUsersUnreadAmount(idUser: string): Promise<number>
-  abstract findManyByUserAndResource(
-    resource: Resource,
-    idUser: string
+  abstract findManyUnreadByUserAndType(
+    idUser: string,
+    type: MetadataType
   ): Promise<Notification[]>
   abstract create(notification: Notification): Promise<void>
 }
--- src/domain/application/use-cases/fetch-user-and-context-notifications-.ts
+++ src/domain/application/use-cases/fetch-user-and-context-notifications-.ts
@@ -1,28 +1,37 @@
-import { Context } from '@/domain/value-objects/context'
+import { Notification } from '@/domain/entities/notification'
+import { MetadataFactory } from '@/domain/value-objects/metadata/metadata-factory'
 import { UseCaseResponse } from '@/types/user-case-response'
 import { NotificationRepository } from '../repositories/notification-repository'
 
 interface FetchUserAndContextNotificationUseCaseRequest {
   recipientId: string
-  context: Context
 }
 
 type FetchUserAndContextNotificationUseCaseResponse = UseCaseResponse<
   {
-    recipientId: string
     unreadAmount: number
+    unreadNotificationsOfType: Notification[]
   },
   Error
 > // TODO: melhorar esse erro
 
 export class FetchUserAndContextNotificationUseCase {
-  constructor(private repository: NotificationRepository) {}
+  constructor(
+    private repository: NotificationRepository,
+    private metadataFactory: MetadataFactory
+  ) {}
 
   async execute({
     recipientId,
   }: FetchUserAndContextNotificationUseCaseRequest): Promise<FetchUserAndContextNotificationUseCaseResponse> {
+    const metadataType = this.metadataFactory.type
     const unreadAmount = await this.repository.getUsersUnreadAmount(recipientId)
+    const unreadNotificationsOfType =
+      await this.repository.findManyUnreadByUserAndType(
+        recipientId,
+        metadataType
+      )
 
-    return [{ recipientId, unreadAmount }, null]
+    return [{ unreadAmount, unreadNotificationsOfType }, null]
   }
 }
