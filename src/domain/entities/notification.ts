038201739f93ad17427ae02fd1c9a4fdf968ef3d
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-24 11:53:42 -0300

    fix: use metadata instead of context

--- src/domain/entities/notification.ts
+++ src/domain/entities/notification.ts
@@ -1,12 +1,12 @@
 import { Optional } from '@/types/optional'
 import { EntityID } from '../value-objects/entity-id'
-import { Context } from '../value-objects/context'
+import { Metadata } from '../value-objects/metadata/notificataion-metadata'
 
 interface NotificationProps {
   recipientId: string
   createdAt: Date
   readAt?: Date | null
-  context: Context
+  metadata: Metadata
 }
 
 export class Notification {
@@ -14,21 +14,17 @@ export class Notification {
   readonly recipientId: string
   private createdAt: Date
   private readAt?: Date | null
-  private context: Context
+  private metadata: Metadata
 
   constructor(props: Optional<NotificationProps, 'createdAt'>, id?: EntityID) {
     this.id = id ?? new EntityID()
     this.recipientId = props.recipientId
     this.createdAt = props.createdAt || new Date()
     this.readAt = props.readAt
-    this.context = props.context
+    this.metadata = props.metadata
   }
 
   public read() {
     this.readAt = new Date()
-  }
-
-  private test() {
-    console.log(this.context.getResourceName())
   }
 }
