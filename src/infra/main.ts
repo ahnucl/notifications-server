442bf6b82ddd58944797797e7d0ca738b904cf19
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-24 13:22:13 -0300

    fix: cleanup

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,10 +1,1 @@
-import { MonitoringItem } from './resources/monitoring-item'
-import { Context } from '../domain/value-objects/context'
-
 console.log('Building...')
-
-console.log(MonitoringItem.name)
-
-const monitoringItemsContext = new Context({ resource: MonitoringItem })
-
-console.log(monitoringItemsContext)
