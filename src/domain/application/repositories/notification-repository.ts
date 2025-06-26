38d9af18a748a86cee6ef0718f56da39a7bfabf8
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-06-25 22:02:42 -0300

    feat: update repository

--- src/domain/application/repositories/notification-repository.ts
+++ src/domain/application/repositories/notification-repository.ts
@@ -1,6 +1,12 @@
 import { Notification } from '@/domain/entities/notification'
 import { MetadataType } from '@/domain/value-objects/metadata/metadata-types'
 
+export interface SearchMetadata {
+  primaryKeyValue: string | number
+  auxiliarFieldValue?: string | number
+  type: MetadataType
+}
+
 export abstract class NotificationRepository {
   abstract findManyByUserId(idUser: string): Promise<Notification[]>
   abstract getUsersUnreadAmount(idUser: string): Promise<number>
@@ -8,5 +14,10 @@ export abstract class NotificationRepository {
     idUser: string,
     type: MetadataType
   ): Promise<Notification[]>
+  abstract findByUserAndMetadata(
+    idUser: string,
+    metadata: SearchMetadata
+  ): Promise<Notification | null>
   abstract create(notification: Notification): Promise<void>
+  abstract save(notification: Notification): Promise<void>
 }
--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -1,4 +1,7 @@
-import { NotificationRepository } from '@/domain/application/repositories/notification-repository'
+import {
+  NotificationRepository,
+  SearchMetadata,
+} from '@/domain/application/repositories/notification-repository'
 import { Notification } from '@/domain/entities/notification'
 import { MetadataType } from '@/domain/value-objects/metadata/metadata-types'
 
@@ -30,7 +33,38 @@ export class InMemoryNotificationRepository extends NotificationRepository {
     )
   }
 
+  async findByUserAndMetadata(
+    idUser: string,
+    { type, primaryKeyValue, auxiliarFieldValue }: SearchMetadata
+  ): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) =>
+        notification.recipientId === idUser &&
+        notification.getType() === type &&
+        notification.primaryId === primaryKeyValue &&
+        notification.auxiliarId === auxiliarFieldValue
+    )
+
+    if (!notification) return null
+
+    return this.cloneInstance(notification) // Needed because altering the refence bypass the persistence (its the same object)
+  }
+
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification)
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const index = this.notifications.findIndex(
+      (item) => item.id === notification.id
+    )
+
+    if (index >= 0) {
+      this.notifications[index] = notification
+    }
+  }
+
+  private cloneInstance(obj: Notification): Notification {
+    return Object.assign(Object.create(Object.getPrototypeOf(obj)), obj)
   }
 }
