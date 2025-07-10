69e3d5864640768ef53876ab75be5ee1a0a78da1
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-07-10 15:01:09 -0300

    feat: function to create notification index

--- src/infra/database/redis/redis.service.ts
+++ src/infra/database/redis/redis.service.ts
@@ -1,12 +1,40 @@
-import { createClient } from 'redis'
+import { createClient, SCHEMA_FIELD_TYPE } from 'redis'
 
 const client = createClient({
   socket: {
-    port: 6379,
+    port: 6380, // use .env
     host: 'localhost',
   },
   database: 0,
 })
+
+export async function createIndex() {
+  await client.ft.create(
+    'idx:notifications',
+    {
+      '$.id': {
+        type: SCHEMA_FIELD_TYPE.TEXT,
+        AS: 'id',
+      },
+      '$.recipientId': {
+        type: SCHEMA_FIELD_TYPE.TEXT,
+        AS: 'recipientId',
+      },
+      '$.metadata.type': {
+        type: SCHEMA_FIELD_TYPE.TEXT,
+        AS: 'type',
+      },
+      '$.readAt': {
+        type: SCHEMA_FIELD_TYPE.TEXT,
+        AS: 'readAt',
+      },
+    },
+    {
+      ON: 'JSON',
+      PREFIX: 'notification:',
+    }
+  )
+}
 
 client.on('error', (error) => {
   console.error(`Redis client error:`, error)
